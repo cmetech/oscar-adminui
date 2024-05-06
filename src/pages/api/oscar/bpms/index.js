ae85f28c9903adf9b3d1340682f31d4957d6f930
Corey Ellis <corey@cmetech.io>
2024-05-06 14:18:47 -0400

    updates

--- src/pages/api/oscar/bpms/index.js
+++ src/pages/api/oscar/bpms/index.js
@@ -2,6 +2,6 @@
 import { getSession } from 'next-auth/react'
 
 export default async function handler(req, res) {
-  const nifiUi = `https://${process.env.DOMAIN}:${process.env.NIFI_PORT}/nifi`
+  const nifiUi = `https://${process.env.NIFI_HOST}:${process.env.NIFI_PORT}/nifi`
   res.redirect(nifiUi)
 }

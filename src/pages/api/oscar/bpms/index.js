9e7bae1668fdee60df06451711498acf0ecd79f4
cmetech <corey@cmetech.io>
2024-06-15 14:00:59 -0400

    updates

--- src/pages/api/oscar/bpms/index.js
+++ src/pages/api/oscar/bpms/index.js
@@ -1,12 +1,26 @@
 // pages/api/redirect.js
 import { getSession } from 'next-auth/react'
 
-// export default async function handler(req, res) {
-//   const nifiUi = `https://${process.env.NIFI_HOST}:${process.env.NIFI_PORT}/nifi`
-//   res.redirect(nifiUi)
-// }
-
 export default async function handler(req, res) {
-  const mageUi = `https://${process.env.MAGEAI_HOST}:${process.env.MAGEAI_PORT}`
-  res.redirect(mageUi)
+  const workflowEngine = process.env.WORKFLOW_ENGINE || 'mageai';
+
+  let redirectUrl;
+
+  switch (workflowEngine) {
+    case 'nifi':
+      redirectUrl = `https://${process.env.NIFI_HOST}:${process.env.NIFI_PORT}/nifi`;
+      break;
+    case 'mageai':
+      redirectUrl = `http://${process.env.MAGEAI_HOST}:${process.env.MAGEAI_PORT}`;
+      break;
+    case 'airflow':
+      redirectUrl = `https://${process.env.AIRFLOW_HOST}:${process.env.AIRFLOW_PORT}`;
+      break;
+    default:
+      // Fallback to mageai if the workflow engine type is invalid
+      redirectUrl = `http://${process.env.MAGEAI_HOST}:${process.env.MAGEAI_PORT}`;
+      break;
+  }
+
+  res.redirect(redirectUrl);
 }

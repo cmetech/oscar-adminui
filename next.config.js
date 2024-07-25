14f5d2e46e62daee0a6af89a2be49dc9e53401cd
cmetech <corey@cmetech.io>
2024-07-25 16:19:05 -0400

    updates

--- next.config.js
+++ next.config.js
@@ -30,7 +30,7 @@ module.exports = {
   // Will be available on both server and client
   publicRuntimeConfig: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    MKDOCS_HOST: process.env.MKDOCS_HOST,
+    MKDOCS_HOST: process.env.DOMAIN,
     MKDOCS_PORT: process.env.MKDOCS_PORT
   },
 

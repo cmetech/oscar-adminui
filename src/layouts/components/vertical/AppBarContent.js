d133668e00e93d756c19083d9de7c2bf510e1d0c
cmetech <corey@cmetech.io>
2024-08-15 14:45:27 -0400

    updates

--- src/layouts/components/vertical/AppBarContent.js
+++ src/layouts/components/vertical/AppBarContent.js
@@ -130,6 +130,7 @@ const CustomBreadcrumbs = () => {
 const AppBarContent = props => {
   // ** Props
   const { hidden, settings, saveSettings, toggleNavVisibility } = props
+  const theme = useTheme()
   const { publicRuntimeConfig } = getConfig()
   const docs_host = publicRuntimeConfig.MKDOCS_HOST || 'localhost'
   const domain = publicRuntimeConfig.DETECTED_IP || 'localhost'

460a64dca98ce17cef02dba278fb07567fc123b7
Corey Ellis <corey@cmetech.io>
2024-10-31 14:08:26 -0400

    updates

--- src/views/pages/connection-management/forms/UpdateConnectionWizard.js
+++ src/views/pages/connection-management/forms/UpdateConnectionWizard.js
@@ -163,16 +163,22 @@ const UpdateConnectionWizard = ({ connectionData, onSuccess }) => {
       host: yup.string().required('Host is required'),
       port: yup.number().typeError('Port must be a number').required('Port is required'),
       schema: yup.string(),
-      login: yup.string().when('conn_type', {
-        is: conn_type => !NO_AUTH_REQUIRED.includes((conn_type || '').toLowerCase()),
-        then: schema => schema.required('Login is required'),
-        otherwise: schema => schema
-      }),
-      password: yup.string().when('conn_type', {
-        is: conn_type => !NO_AUTH_REQUIRED.includes((conn_type || '').toLowerCase()),
-        then: schema => schema.required('Password is required'),
-        otherwise: schema => schema
-      }),
+      login: yup
+        .string()
+        .nullable()
+        .when('conn_type', {
+          is: conn_type => !NO_AUTH_REQUIRED.includes((conn_type || '').toLowerCase()),
+          then: schema => schema.required('Login is required'),
+          otherwise: schema => schema
+        }),
+      password: yup
+        .string()
+        .nullable()
+        .when('conn_type', {
+          is: conn_type => !NO_AUTH_REQUIRED.includes((conn_type || '').toLowerCase()),
+          then: schema => schema.required('Password is required'),
+          otherwise: schema => schema
+        }),
       description: yup.string()
     }),
 

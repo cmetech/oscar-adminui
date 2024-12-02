89dfeccab66fe10721c0d8be883fc7848b273e44
Corey Ellis <corey@cmetech.io>
2024-12-02 07:41:23 -0500

    updates for alert reloading

--- src/pages/observability/alerts/index.js
+++ src/pages/observability/alerts/index.js
@@ -20,8 +20,21 @@ import Grid from '@mui/material/Grid'
 import Tab from '@mui/material/Tab'
 import TabContext from '@mui/lab/TabContext'
 import TabPanel from '@mui/lab/TabPanel'
-import { Box, Typography, TextField } from '@mui/material'
+import {
+  Box,
+  Typography,
+  TextField,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions
+} from '@mui/material'
 import toast from 'react-hot-toast'
+import axios from 'axios'
 
 // ** Styled Component Imports
 import { styled, useTheme } from '@mui/material/styles'
@@ -61,6 +74,71 @@ const TabList = styled(MuiTabList)(({ theme }) => ({
   }
 }))
 
+// ** MoreActionsDropdown Component
+const MoreActionsDropdown = ({ onAction }) => {
+  const [anchorEl, setAnchorEl] = useState(null)
+  const { t } = useTranslation()
+  const theme = useTheme()
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const handleMenuItemClick = action => {
+    onAction(action)
+    handleClose()
+  }
+
+  const menuItemStyles = {
+    py: 2,
+    px: 4,
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    color: theme.palette.text.primary,
+    textDecoration: 'none',
+    '& svg': {
+      mr: 2,
+      fontSize: '1.375rem',
+      color: theme.palette.text.primary
+    }
+  }
+
+  return (
+    <Fragment>
+      <IconButton color='secondary' onClick={handleClick}>
+        <Icon icon='mdi:menu' />
+      </IconButton>
+      <Menu
+        id='more-actions-menu'
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        sx={{ '& .MuiMenu-paper': { width: 230, mt: 4 } }}
+      >
+        <MenuItem sx={{ p: 0 }} onClick={() => handleMenuItemClick('reload_configuration')}>
+          <Box sx={menuItemStyles}>
+            <Icon icon='mdi:reload' />
+            {t('Reload Configuration')}
+          </Box>
+        </MenuItem>
+        <MenuItem sx={{ p: 0 }} onClick={() => handleMenuItemClick('reload_rules')}>
+          <Box sx={menuItemStyles}>
+            <Icon icon='mdi:reload' />
+            {t('Reload Rules')}
+          </Box>
+        </MenuItem>
+      </Menu>
+    </Fragment>
+  )
+}
+
+// ** Main Component
 const Alerts = () => {
   // ** Hooks
   const ability = useContext(AbilityContext)
@@ -79,6 +157,9 @@ const Alerts = () => {
 
   // ** Get extended predefined ranges
   const extendedPredefinedRangesDayjs = getExtendedPredefinedRangesDayjs(timezone, t)
+
+  const [confirmationDialogOpen, setConfirmationDialogOpen] = useState(false)
+  const [actionToConfirm, setActionToConfirm] = useState('')
 
   const handleChange = (event, newValue) => {
     setValue(newValue)
@@ -123,135 +204,181 @@ const Alerts = () => {
     return dateRange[0] ? dayjs(dateRange[0]).add(12, 'hour') : null
   }
 
+  const handleMoreActions = action => {
+    setActionToConfirm(action)
+    setConfirmationDialogOpen(true)
+  }
+
+  const handleConfirmAction = async () => {
+    try {
+      let url = ''
+      if (actionToConfirm === 'reload_configuration') {
+        url = '/api/alertmanager'
+      } else if (actionToConfirm === 'reload_rules') {
+        url = '/api/alertmanager/rules'
+      }
+
+      await axios.post(url)
+      toast.success(t(`${actionToConfirm.replace('_', ' ')} successful`))
+    } catch (error) {
+      console.error(`Failed to ${actionToConfirm}`, error)
+      toast.error(t(`Failed to ${actionToConfirm}`))
+    } finally {
+      setConfirmationDialogOpen(false)
+    }
+  }
+
   return (
     <Grid container spacing={6}>
       <Grid item xs={12}>
         <Box display='flex' justifyContent='space-between' alignItems='center' mb={6}>
           <Typography variant='h4'>{t('Alert Management')}</Typography>
-          {value === '2' && (
-            <DateTimeRangePicker
-              calendars={2}
-              closeOnSelect={false}
-              value={dateRange}
-              views={['day', 'hours']}
-              timeSteps={{ minute: 10 }}
-              viewRenderers={{ hours: renderDigitalClockTimeView }}
-              onChange={newValue => {
-                // console.log('Date range:', newValue)
-                setDateRange(newValue)
-              }}
-              onAccept={handleOnAccept}
-              slotProps={{
-                field: { dateSeparator: 'to' },
-                textField: ({ position }) => ({
-                  size: 'small',
-                  color: position === 'start' ? 'secondary' : 'secondary',
-                  focused: true,
-                  InputProps: {
-                    endAdornment: <Icon icon='mdi:calendar' />
-                  }
-                }),
-                desktopPaper: {
-                  style: {
-                    backgroundColor:
-                      theme.palette.mode === 'dark' ? theme.palette.primary.dark : theme.palette.common.white
-                  }
-                },
+          <MoreActionsDropdown onAction={handleMoreActions} />
+        </Box>
 
-                day: {
-                  sx: {
-                    '& .MuiPickersDay-root': {
-                      color: theme.palette.mode === 'dark' ? theme.palette.common.white : theme.palette.common.black,
-                      '&:hover': {
-                        color: theme.palette.customColors.accent
-                      }
-                    },
-                    '& .MuiPickersDay-root.Mui-selected': {
-                      color: theme.palette.mode === 'dark' ? theme.palette.common.white : theme.palette.common.white
+        {/* Confirmation Dialog */}
+        <Dialog
+          open={confirmationDialogOpen}
+          onClose={() => setConfirmationDialogOpen(false)}
+          aria-labelledby='confirmation-dialog-title'
+        >
+          <DialogTitle id='confirmation-dialog-title'>{t('Confirm Action')}</DialogTitle>
+          <DialogContent>
+            <Typography>{t(`Are you sure you want to ${actionToConfirm.replace('_', ' ')}?`)}</Typography>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setConfirmationDialogOpen(false)} variant='outlined' color='secondary'>
+              {t('Cancel')}
+            </Button>
+            <Button onClick={handleConfirmAction} variant='contained' color='primary'>
+              {t('Confirm')}
+            </Button>
+          </DialogActions>
+        </Dialog>
+
+        {value === '2' && (
+          <DateTimeRangePicker
+            calendars={2}
+            closeOnSelect={false}
+            value={dateRange}
+            views={['day', 'hours']}
+            timeSteps={{ minute: 10 }}
+            viewRenderers={{ hours: renderDigitalClockTimeView }}
+            onChange={newValue => {
+              // console.log('Date range:', newValue)
+              setDateRange(newValue)
+            }}
+            onAccept={handleOnAccept}
+            slotProps={{
+              field: { dateSeparator: 'to' },
+              textField: ({ position }) => ({
+                size: 'small',
+                color: position === 'start' ? 'secondary' : 'secondary',
+                focused: true,
+                InputProps: {
+                  endAdornment: <Icon icon='mdi:calendar' />
+                }
+              }),
+              desktopPaper: {
+                style: {
+                  backgroundColor:
+                    theme.palette.mode === 'dark' ? theme.palette.primary.dark : theme.palette.common.white
+                }
+              },
+
+              day: {
+                sx: {
+                  '& .MuiPickersDay-root': {
+                    color: theme.palette.mode === 'dark' ? theme.palette.common.white : theme.palette.common.black,
+                    '&:hover': {
+                      color: theme.palette.customColors.accent
                     }
+                  },
+                  '& .MuiPickersDay-root.Mui-selected': {
+                    color: theme.palette.mode === 'dark' ? theme.palette.common.white : theme.palette.common.white
                   }
-                },
+                }
+              },
 
-                shortcuts: {
-                  items: extendedPredefinedRangesDayjs,
-                  sx: {
-                    '& .MuiChip-root': {
+              shortcuts: {
+                items: extendedPredefinedRangesDayjs,
+                sx: {
+                  '& .MuiChip-root': {
+                    color:
+                      theme.palette.mode === 'dark'
+                        ? theme.palette.customColors.brandYellow
+                        : theme.palette.primary.main,
+                    '&:hover': {
                       color:
                         theme.palette.mode === 'dark'
                           ? theme.palette.customColors.brandYellow
                           : theme.palette.primary.main,
-                      '&:hover': {
-                        color:
-                          theme.palette.mode === 'dark'
-                            ? theme.palette.customColors.brandYellow
-                            : theme.palette.primary.main,
-                        backgroundColor:
-                          theme.palette.mode === 'dark' ? theme.palette.secondary.dark : theme.palette.secondary.light
-                      }
+                      backgroundColor:
+                        theme.palette.mode === 'dark' ? theme.palette.secondary.dark : theme.palette.secondary.light
                     }
                   }
-                },
+                }
+              },
 
-                digitalClockItem: {
-                  sx: {
+              digitalClockItem: {
+                sx: {
+                  '&:hover': {
+                    color:
+                      theme.palette.mode === 'dark'
+                        ? theme.palette.customColors.brandBlack
+                        : theme.palette.customColors.black,
+                    background:
+                      theme.palette.mode == 'dark'
+                        ? theme.palette.customColors.brandGray4
+                        : theme.palette.customColors.brandGray4
+                  },
+                  '&.Mui-selected': {
+                    background:
+                      theme.palette.mode == 'dark'
+                        ? theme.palette.customColors.brandYellow4
+                        : theme.palette.customColors.brandGray1
+                  }
+                }
+              },
+
+              actionBar: {
+                actions: ['clear', 'today', 'cancel', 'accept'],
+                sx: {
+                  '& .MuiDialogActions-root, .MuiButton-root': {
+                    // Targeting buttons inside MuiDialogActions-root
+                    borderWidth: '1px', // Ensure there's a visible border
+                    borderStyle: 'solid', // Necessary for the border to show
+                    borderColor:
+                      theme.palette.mode === 'dark'
+                        ? theme.palette.customColors.brandGray1b
+                        : theme.palette.primary.main,
+                    color:
+                      theme.palette.mode === 'dark'
+                        ? theme.palette.customColors.brandWhite
+                        : theme.palette.primary.main,
                     '&:hover': {
-                      color:
-                        theme.palette.mode === 'dark'
-                          ? theme.palette.customColors.brandBlack
-                          : theme.palette.customColors.black,
-                      background:
-                        theme.palette.mode == 'dark'
-                          ? theme.palette.customColors.brandGray4
-                          : theme.palette.customColors.brandGray4
-                    },
-                    '&.Mui-selected': {
-                      background:
-                        theme.palette.mode == 'dark'
-                          ? theme.palette.customColors.brandYellow4
-                          : theme.palette.customColors.brandGray1
-                    }
-                  }
-                },
-
-                actionBar: {
-                  actions: ['clear', 'today', 'cancel', 'accept'],
-                  sx: {
-                    '& .MuiDialogActions-root, .MuiButton-root': {
-                      // Targeting buttons inside MuiDialogActions-root
-                      borderWidth: '1px', // Ensure there's a visible border
-                      borderStyle: 'solid', // Necessary for the border to show
+                      backgroundColor: 'rgba(0, 0, 255, 0.04)', // Custom background color on hover
                       borderColor:
-                        theme.palette.mode === 'dark'
-                          ? theme.palette.customColors.brandGray1b
-                          : theme.palette.primary.main,
-                      color:
                         theme.palette.mode === 'dark'
                           ? theme.palette.customColors.brandWhite
                           : theme.palette.primary.main,
-                      '&:hover': {
-                        backgroundColor: 'rgba(0, 0, 255, 0.04)', // Custom background color on hover
-                        borderColor:
-                          theme.palette.mode === 'dark'
-                            ? theme.palette.customColors.brandWhite
-                            : theme.palette.primary.main,
-                        color:
-                          theme.palette.mode === 'dark'
-                            ? theme.palette.customColors.brandYellow
-                            : theme.palette.primary.main
-                      }
+                      color:
+                        theme.palette.mode === 'dark'
+                          ? theme.palette.customColors.brandYellow
+                          : theme.palette.primary.main
                     }
                   }
-                },
-                endDesktopDateTimePicker: {
-                  maxDateTime: getMaxEndDateTime()
-                },
-                endMobileDateTimePicker: {
-                  maxDateTime: getMaxEndDateTime()
                 }
-              }}
-            />
-          )}
-        </Box>
+              },
+              endDesktopDateTimePicker: {
+                maxDateTime: getMaxEndDateTime()
+              },
+              endMobileDateTimePicker: {
+                maxDateTime: getMaxEndDateTime()
+              }
+            }}
+          />
+        )}
         <TabContext value={value}>
           <TabList onChange={handleChange} aria-label='Alert-tabs'>
             {activeAlertsTotal == 0 ? (

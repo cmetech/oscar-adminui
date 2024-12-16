06ad54d7100c3b88cc2d74ab09287d44620e46dd
amukh93 <mukherjeeayan952@gmail.com>
2024-12-17 04:19:55 +0530

    update frontend

--- src/configs/oscarConfig.js
+++ src/configs/oscarConfig.js
@@ -4,6 +4,7 @@ const oscarConfig = {
   MIDDLEWARE_API_URL: `${process.env.NEXT_PUBLIC_API_URL}/api/v1`,
   MIDDLWARE_METRICSTORE_API_URL: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/metricstore`,
   MIDDLEWARE_INVENTORY_API_URL: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/inventory`,
+  MIDDLEWARE_MAPPING_API_URL: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/mapping-data`,
   BRANDING_TAGLINE: 'powered by SMART2D'
 }
 
